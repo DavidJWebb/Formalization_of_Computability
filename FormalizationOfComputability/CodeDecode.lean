aca72f77dec4029bd2390e77d19dbf792a933169
David J. Webb <david.webb@chaminade.edu>
2025-05-30 03:16:56 -1000

    added encode_ofNatCode

--- FormalizationOfComputability/CodeDecode.lean
+++ FormalizationOfComputability/CodeDecode.lean
@@ -9,37 +9,49 @@ import Mathlib.Tactic.Linarith
 
 set_option warningAsError false
 
-/-
-# Encoding and Decoding
-A lemma that encoding and decoding a natural number results in
-the same partial recursive function, so that the two truly are
-inverses of one another
--/
 
 namespace Nat.Partrec.Code
 
-lemma encode_inverse_eval (c : Code) :
-    c = (Code.ofNatCode (Code.encodeCode c))  := by
+/- Helper lemmas that ofNatCode and encodeCode are inverse functions. The latter
+is present in Partrec.Code as a private -/
+lemma ofNatCode_encode (c : Code) :
+    c = (ofNatCode (encodeCode c)) := by
   induction c with
-  | zero => simp [Code.encodeCode, Code.ofNatCode, Code.eval]
-  | succ => simp [Code.encodeCode, Code.ofNatCode, Code.eval]
-  | left => simp [Code.encodeCode, Code.ofNatCode, Code.eval]
-  | right => simp [Code.encodeCode, Code.ofNatCode, Code.eval]
+  | zero => simp [encodeCode, ofNatCode]
+  | succ => simp [encodeCode, ofNatCode]
+  | left => simp [encodeCode, ofNatCode]
+  | right => simp [encodeCode, ofNatCode]
   | pair cf cg ihf ihg =>
-      simp [Code.encodeCode, Code.ofNatCode, Code.eval]
-      constructor
-      · exact ihf
-      · exact ihg
+      simp [encodeCode, ofNatCode]
+      simp [← ihf, ← ihg]
   | comp cf cg ihf ihg =>
-      simp [Code.encodeCode, Code.ofNatCode, Code.eval]
-      constructor
-      · exact ihf
-      · exact ihg
+      simp [encodeCode, ofNatCode]
+      simp [← ihf, ← ihg]
   | prec cf cg ihf ihg =>
-      simp [Code.encodeCode, Code.ofNatCode, Code.eval]
-      constructor
-      · exact ihf
-      · exact ihg
+      simp [encodeCode, ofNatCode]
+      simp [← ihf, ← ihg]
   | rfind' cf ihf =>
-      simp [Code.encodeCode, Code.ofNatCode, Code.eval]
+      simp [encodeCode, ofNatCode]
       exact ihf
+
+lemma encode_ofNatCode : ∀ n, encodeCode (ofNatCode n) = n
+  | 0 => by simp [ofNatCode, encodeCode]
+  | 1 => by simp [ofNatCode, encodeCode]
+  | 2 => by simp [ofNatCode, encodeCode]
+  | 3 => by simp [ofNatCode, encodeCode]
+  | n + 4 => by
+    let m := n.div2.div2
+    have hm : m < n + 4 := by
+      simp only [m, div2_val]
+      exact
+        lt_of_le_of_lt (le_trans (Nat.div_le_self _ _) (Nat.div_le_self _ _))
+          (Nat.succ_le_succ (Nat.le_add_right _ _))
+    have _m1 : m.unpair.1 < n + 4 := lt_of_le_of_lt m.unpair_left_le hm
+    have _m2 : m.unpair.2 < n + 4 := lt_of_le_of_lt m.unpair_right_le hm
+    have IH := encode_ofNatCode m
+    have IH1 := encode_ofNatCode m.unpair.1
+    have IH2 := encode_ofNatCode m.unpair.2
+    conv_rhs => rw [← Nat.bit_decomp n, ← Nat.bit_decomp n.div2]
+    simp only [ofNatCode.eq_5]
+    cases n.bodd <;> cases n.div2.bodd <;>
+      simp [m, encodeCode, ofNatCode, IH, IH1, IH2, Nat.bit_val]
